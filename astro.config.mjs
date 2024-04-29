bc860abcd568f4df8a7bbccfb3066e597936fc7e
matem <matemaki33@gmail.com>
2024-04-29 13:27:35 +0200

    carga de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  output: "server",
   site: 'https://MateMaki33.github.io',
   
 });
