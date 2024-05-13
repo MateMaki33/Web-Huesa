b222de09618071f509c394e493f9b92fed35f3e3
matem <matemaki33@gmail.com>
2024-05-13 09:10:26 +0200

    a ver si ya podemos subir a github

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   output: "server",
   site: 'https://MateMaki33.github.io',
-  base: 'https://matemaki33.github.io'
-  
+  base: 'https://matemaki33.github.io',
+  adapter: node({
+    mode: "standalone"
+  })
 });
