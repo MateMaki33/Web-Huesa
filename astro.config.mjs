e7666f77ee30676cbc9a4edbcd884c7655630e1d
matemaki33 <93308327+MateMaki33@users.noreply.github.com>
2024-05-13 10:25:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import node from "@astrojs/node";
 export default defineConfig({
   integrations: [vue()],
   output: "server",
-  site: 'https://matemaki33.github.io',
+  site: 'https://github.com/MateMaki33/MateMaki33.github.io',
   adapter: node({
     mode: "standalone"
   })
