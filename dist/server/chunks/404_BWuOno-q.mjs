04f79b186aa52d470e3283ead658650ddd75f432
matemaki33 <93308327+MateMaki33@users.noreply.github.com>
2024-05-14 19:04:30 +0200

    Delete dist directory

--- dist/server/chunks/404_BWuOno-q.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_yR3XJ1zS.mjs').then(n => n.b);
-
-export { page };
--- dist/server/chunks/ayuntamiento_BJipB6Yf.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/ayuntamiento_bV26DzQ5.mjs').then(n => n.d);
-
-export { page };
